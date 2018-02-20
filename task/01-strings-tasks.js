59c26ab2f6bf440594ab4626421b402239856c32
SergueiDementchouk <widzew@tut.by>
2018-02-21 00:19:48 +0300

    Update 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -40,6 +40,7 @@ function concatenateStrings(value1, value2) {
  */
 function getStringLength(value) {
    /* throw new Error('Not implemented');*/
+   return value.length;
 }
 
 /**
@@ -56,7 +57,8 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+  throw new Error('Not implemented');
+   
 }
 
 /**
